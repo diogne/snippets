86915993210dfc3feb9e139381e68bac69ed7462
Jérôme <jerome.riou@gmail.com>
2021-12-06 06:42:18 +0100

    bash

---
+++ script_lancement_lightsail.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install python3-venv
+git clone XXXXXXX
+cd XXXXXXXXXXX
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade  pip
+pip install -r requirements.txt
