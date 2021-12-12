ade565ed3f1be161ce2a278c637e4f8a5ed996c0
Jérôme <jerome.riou@gmail.com>
2021-12-13 00:14:01 +0100

    idée

--- script_lancement_lightsail.sh
+++ script_lancement_lightsail.sh
@@ -6,3 +6,11 @@ python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade  pip
 pip install -r requirements.txt
+
+
+
+
+python3 -m venv venv && source venv/bin/activate && pip install --upgrade  pip
+
+
+sudo apt install python3 && sudo apt install python3-pip && pip3 install --upgrade pip && pip3 install --upgrade pip && sudo apt install python3-venv && python3 -m venv venv && source venv/bin/activate
